2aa5dc3538a107ea71a76e42e079e82c7bbbcbb3
= <=>
2016-10-12 20:42:10 +0200

    promises implementation completed

--- controller.js
+++ controller.js
@@ -6,18 +6,18 @@
 const messagesModel = require('./model.js');
 
 exports.getLatest = function (req, res) {
-  messagesModel.getLatest(5, function (err, data){
-    if (err) res.send(400);  //  TODO:  write more logic for this
-    else res.json(data);
+  messagesModel.getLatest(5).then(function (value) {
+    //  TODO:  write more logic for this
+    res.json(value);    //  Success!
+  }, function (reason) {
+    res.send(reason);   //  Error!
   });
 };
 //  we want to update this to implement a call back so it does not return undefined
 exports.create = function (req, res) {
-  messagesModel.set(Date.now(), req.body.content, function (err, data) {
-    if (err) res.send(400);  //  TODO from above
-    else {
-      res.json(data);
-      // res.send(200);
-    }
+  messagesModel.set(Date.now(), req.body.content).then(function (value){
+    res.json(value);  //  Success!
+  }, function (reason) {
+    res.send(reason);  //  Error!  TODO for both extend functionality
   });
 };
--- model.js
+++ model.js
@@ -12,28 +12,23 @@ const connection = mysql.createConnection({
 
 const tableName = 'messages';
 
-exports.getLatest = function (num, cb) {
-  connection.query(`SELECT *
-                    FROM ${tableName}
-                    posting ORDER BY timestamp DESC
-                    LIMIT ${num}`, function(err, rows, fields) {
-    if (err) cb(err);
-    cb(null, rows);
+exports.getLatest = function (num) {
+  return new Promise (function (resolve, reject) {
+    connection.query(`SELECT * FROM ${tableName} posting ORDER BY timestamp DESC LIMIT ${num}`,
+       function(err, rows, fields) {
+        if (err) reject(err);
+        resolve(rows);
+    });
   });
 };
 
 
-exports.set = function (timestamp, content, cb) {
-  connection.query(`INSERT into ${tableName} (timestamp, content) values (${timestamp}, '${content}')`,
-  //  try use query .on( 'result ') here from https://github.com/mysqljs/mysql
-  function (err, data){
-    console.log(data);
-    if (err) cb(err);
-    else cb (null, {
-      id: data.insertId,
-      timestamp: timestamp,
-      content: content
+exports.set = function (timestamp, content) {
+  return new Promise (function (resolve, reject) {
+    connection.query(`INSERT into ${tableName} (timestamp, content) values (${timestamp}, '${content}')`,
+    function (err, data){
+      if (err) reject(err);
+      else resolve({id: data.insertId, timestamp: timestamp, content: content});
     });
   });
-  // return undefined;
 };
